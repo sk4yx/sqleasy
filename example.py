ea136bf9c40092c7ce5719a25cc48c25a4375333
sk4yx <kaneki.liou@gmail.com>
2022-04-25 19:06:19 -0300

    Update example.py

--- example.py
+++ example.py
@@ -2,20 +2,20 @@ import sqleasy
 import time
 
 '''
-hey guys, this is sql easy
+this is sqleasy
 '''
 
 sqleasy.connect("database")
 sqleasy.createTable("database", "users", "user TEXT, password TEXT, sqleasy BOOLEAN")
 sqleasy.insertValue("database", "users", "user, password", "'sqleasy', 'password'")
-sqleasy.getValue("database", "*", "users")
+print(sqleasy.getValue("database", "*", "users"))
 print("")
 time.sleep(1)
 sqleasy.changeValue("database", "users", "user", "'sqleasyiscool'", "user", "sqleasy")
-sqleasy.getValue("database", "user", "users")
+print(sqleasy.getValue("database", "user", "users"))
 print("")
 sqleasy.deleteValue("database", "users", "user", "'sqleasyiscool'")
-sqleasy.getValue("database", "user", "users")
+print(sqleasy.getValue("database", "user", "users"))
 print("")
 sqleasy.addColumnInTable("database", "users", "sqleasyiscool BOOLEAN")
 sqleasy.deleteTable("database", "users")
